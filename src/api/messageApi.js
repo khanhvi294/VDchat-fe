06898e7e500f002a650176a3f7b9be4706c0514d
Duc Nguyen <ntrungduc228@gmail.com>
2023-04-29 14:57:43 +0700

    complete preview fileslist

--- src/api/messageApi.js
+++ src/api/messageApi.js
@@ -6,6 +6,12 @@ const messageApi = {
     const paramsString = generateParamsString({ page, limit });
     return axiosClientPrivate.get(`${url}/${conversationId}?${paramsString}`);
   },
+  createMessage: (data) => {
+    console.log("datasemd ", data);
+    return axiosClientPrivate.post(`${url}`, data, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+  },
 };
 
-export const { getMessages } = messageApi;
+export const { getMessages, createMessage } = messageApi;
--- src/features/chat/ChatFooter.jsx
+++ src/features/chat/ChatFooter.jsx
@@ -6,87 +6,97 @@ import {
   LinkOutlined,
   FileOutlined,
   CloseOutlined,
+  SendOutlined,
 } from "@ant-design/icons";
 import React, { useState } from "react";
 import { Button, Image, Upload } from "antd";
-
+import PreviewFileList from "../../component/PreviewFilesList";
 import { Input } from "antd";
 import { Footer } from "antd/es/layout/layout";
+import { createMessage } from "../../api/messageApi";
+import { useMutation } from "@tanstack/react-query";
+import createUrlImage from "../../utils/createUrlImg";
 
 const ChatFooter = () => {
-  const [imgList, setImgList] = useState([]);
+  const [fileList, setFileList] = useState([]);
 
-  const propUpload = {
-    onChange({ file, fileList }) {
-      if (file.status !== "uploading") {
-        console.log(file, fileList);
-      }
-    },
+  const createMessageMutation = useMutation(createMessage, {});
+
+  const handleRemoveFileChosen = (indexFile) => {
+    setFileList((prev) => prev.filter((file) => file.key !== indexFile));
   };
 
-  return (
-    <Footer className="bg-white border-t-2 h-[200px] sticky bottom-0">
-      <div className="flex rounded-xl bg-[#e6ebf5] flex-col justify-between items-center">
-        {/* <div className=" flex  flex-col justify-between items-center w-full h-full hover:border-[#a49eed]">
-         
-        </div> */}
-        <div className="flex-1 bg-teal-200 w-full p-2  rounded-xl flex">
-          <Upload>
-            <Button
-              className="mr-3 bg-slate-400 flex justify-center items-center"
-              icon={<PlusCircleOutlined />}
-              size={"large"}
-            />
-          </Upload>
+  const convertBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
 
-          <div className="flex flex-1 gap-3 overflow-x-scroll">
-            <div className="relative">
-              <Button
-                shape="circle"
-                className=" absolute z-10 top-0 -right-2 bg-slate-700 flex items-center justify-center "
-                icon={<CloseOutlined />}
-                size={"small"}
-                onClick={() => alert("foskfosd")}
-              />
-              <Image
-                preview={false}
-                className=" rounded-lg"
-                width={60}
-                src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-              />
-            </div>{" "}
-            <div className="relative">
-              <Button
-                shape="circle"
-                className=" absolute z-10 top-0 -right-2 bg-slate-700 flex items-center justify-center "
-                icon={<CloseOutlined />}
-                size={"small"}
-                onClick={() => alert("foskfosd")}
-              />
-              <Image
-                preview={false}
-                className=" rounded-lg"
-                width={60}
-                src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-              />
-            </div>
-            <div className="relative">
-              <Button
-                shape="circle"
-                className=" absolute z-10 top-0 -right-2 bg-slate-700 flex items-center justify-center "
-                icon={<CloseOutlined />}
-                size={"small"}
-                onClick={() => alert("foskfosd")}
-              />
-              <div className="rounded-xl w-fit bg-slate-500 flex break-all items-center justify-center max-w-[230px] p-2">
-                <FileOutlined className="text-white" />
-                <p className="text-white ml-2 truncate">
-                  fdgfdgdfgdfgdfgfdgfdgdfgdfgdfgfdgfdgdfgdfgdfg.txt
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+
+  const handleSubmitInput = async () => {
+    if (fileList.length) {
+      // let base64s = await Promise.all(
+      //   fileList.map(async (file) => await convertBase64(file.file))
+      // );
+      // console.log("base64s", base64s);
+      // createMessageMutation.mutate(base64s);
+      const formData = new FormData();
+      // fileList.forEach((file) => {
+      //   formData.append("file", file.file);
+      // });
+      // formData.append("files[]", fileList);
+      formData.append("file", fileList[0]);
+      // formData.append("fs", "sfd");
+
+      console.log("formdata", formData);
+      for (var key of formData.entries()) {
+        console.log(key[0] + ", " + key[1]);
+      }
+      createMessageMutation.mutate(formData);
+    }
+  };
+
+  const handleFileInputChange = (e) => {
+    let files = [];
+    let index = fileList.length || -1;
+    for (let i = 0; i < e.target.files.length; i++) {
+      files.push({
+        key: ++index,
+        url:
+          e.target.files[i].type.includes("image") &&
+          createUrlImage(e.target.files[i]),
+        file: e.target.files[i],
+        type: e.target.files[i].type.includes("image") ? "image" : "file",
+        name: e.target.files[i].name,
+      });
+    }
+    setFileList([...fileList, ...files]);
+  };
+  // console.log("fileList ", fileList);
+
+  return (
+    <Footer
+      className={`bg-white border-t-2 {${
+        fileList.length ? "h-[200px]" : "h-20"
+      }}} sticky bottom-0`}
+    >
+      <div
+        className={`flex rounded-xl ${
+          fileList.length ? "h-full" : ""
+        }  bg-[#e6ebf5] flex-col flex-1 justify-between items-center`}
+      >
+        <PreviewFileList
+          fileList={fileList}
+          handleRemoveFileChosen={handleRemoveFileChosen}
+        />
         <div className="flex w-full items-center">
           <Input
             size="large"
@@ -94,11 +104,37 @@ const ChatFooter = () => {
             placeholder="Message"
             className="rounded-b-xl bg-[#e6ebf5] hover:bg-[#e6ebf5] focus:bg-[#e6ebf5] text-black "
           />
-          <div className="flex justify-around mx-3 gap-3">
-            <SmileOutlined style={{ fontSize: "22px", color: "#a49eed" }} />
-            <Upload {...propUpload}>
+          <div className="flex items-center justify-around mx-3 gap-3">
+            {/* <SendOutlined className="hover:cursor-pointer text-[22px] text-[#a49eed]" /> */}
+            <Button
+              type="text"
+              icon={
+                <SendOutlined className="hover:cursor-pointer text-[22px] text-[#a49eed]" />
+              }
+              onClick={handleSubmitInput}
+            />
+            <SmileOutlined className="hover:cursor-pointer text-[22px] text-[#a49eed]" />
+            <div className="">
+              <label htmlFor="file-input" className="hover:cursor-pointer">
+                <LinkOutlined className="hover:cursor-pointer text-[22px] text-[#a49eed]" />
+              </label>
+              <input
+                onChange={handleFileInputChange}
+                id="file-input"
+                type="file"
+                hidden
+                multiple
+              />
+            </div>
+            {/* <Upload 
+              {...propUpload}
+              showUploadList={false}
+              multiple={true}
+              // fileList={fileList}
+              onChange={handleChangeFileList}
+            >
               <LinkOutlined style={{ fontSize: "22px", color: "#a49eed" }} />
-            </Upload>
+            </Upload> */}
           </div>
         </div>
       </div>
